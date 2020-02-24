c2e025bbb2d0ed906581d06561f0b4b42739c013
techGuto <61423795+techGuto@users.noreply.github.com>
2020-02-24 19:35:07 -0300

    Create app.js

---
+++ project/js/app.js
@@ -0,0 +1,12 @@
+var btnSignIn = document.querySelector("#signin");
+var btnSignUp = document.querySelector("#signup");
+var body = document.querySelector("body")
+
+
+btnSignIn.addEventListener("click", function(){
+    body.className = "sign-in-js";
+});
+
+btnSignUp.addEventListener("click", function(){
+    body.className = "sign-up-js"
+});
